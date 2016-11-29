f7aec2f7a8b025c2a0e30ee6da1aeb26d7529add
Christopher Mattox <metroplex02@gmail.com>
2016-11-29 14:04:57 -0600

    First Commit

---
+++ server/public/scripts/controllers/dogs.controller.js
@@ -0,0 +1,29 @@
+app.controller("DogController", ["$http", function($http) {
+  console.log('dogController up and running');
+  var key = 'adb8927f6ae7945e243d047a6d5e1a7c';
+  var self = this;
+
+  self.animal = {};
+
+  self.getRandomPet = function(){
+    var query = 'http://api.petfinder.com/'; // baseURL for API
+    query += 'pet.getRandom'; // selecting the method we would like to return
+    query += '?key=' + key; // Giving petfinder our key
+    query += '&animal=' + 'dog';
+    query += '&format=json'; // Telling petfinder we want our response to be JSON
+    query += '&output=basic'; // Telling petfinder what data we want (more than just id)
+    var request = encodeURI(query) + '&callback=JSON_CALLBACK'; // removing spaces and special characters from response as well as making jsonp work with the callback
+
+    console.log('Request:', request);
+
+    $http.jsonp(request).then(function(response){
+      console.log(response);
+      self.animal = response.data.petfinder.pet;
+    });
+
+  }
+
+  self.getRandomPet();
+
+  self.message = "Woof!"
+}]);
---
+++ server/routes/cats.js
@@ -0,0 +1,5 @@
+var router = require('express').Router();
+var pg = require('pg');
+var config = require('../config/dbconfig');
+
+module.exports = router;
